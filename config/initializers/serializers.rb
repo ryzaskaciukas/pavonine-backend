d4e059c72d16c2cb7261f8079cc8462ba424a5a0
Domas <domas.bitvinskas@me.com>
2014-10-29 12:09:32 +0200

    Send relationships along with the record

--- config/initializers/serializers.rb
+++ config/initializers/serializers.rb
@@ -7,14 +7,21 @@ ActiveModel::ArraySerializer.root = false
 
 ActiveRecord::Base.descendants.each do |model|
   klass = Class.new(ActiveModel::Serializer) do
-    relations = []
+    keywords = %w(relationships)
+    class_variable_set(:@@relationships, [])
+
+    def relationships
+      self.class.class_variable_get(:@@relationships)
+    end
 
     model.column_names.each do |column|
       match = column.match(/(.+?)_id/)
       next unless match
 
       with_id, name = match[0], match[1]
+      relations = class_variable_get(:@@relationships)
       relations << name
+      class_variable_set(:@@relationships, relations)
 
       define_method(name) do
         relation = name.capitalize.constantize
@@ -23,7 +30,11 @@ ActiveRecord::Base.descendants.each do |model|
       end
     end
 
-    attributes(*(model.column_names + relations))
+    attributes(*(
+      model.column_names +
+      class_variable_get(:@@relationships) +
+      keywords
+    ))
   end
   Object.const_set("#{model.name}Serializer", klass)
 end
