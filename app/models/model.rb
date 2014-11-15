cfb21ff9e7bc54cec83354e7e695feef82d30220
Domas <domas.bitvinskas@me.com>
2014-11-15 12:14:09 +0200

    Add timestamps and automagically detect int attributes

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,8 +2,17 @@ class Model
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
   include Mongoid::Token
+  include Mongoid::Timestamps
 
   field :model, type: String
   field :app_token, type: String
   token
+
+  before_save do
+    attributes.each do |attribute, value|
+      if /\A[-+]?\d+\z/ === value
+        self[attribute] = value.to_i
+      end
+    end
+  end
 end
